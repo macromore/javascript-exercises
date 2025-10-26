402d41bf60315cf6991859ab000d812e01a4e68d
David Blessing <8893118+macromore@users.noreply.github.com>
2025-10-25 21:49:44 -0400

    Complete ex07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    arr.reverse();
+    let str = "";
+    for (let item of arr){
+        str += item;
+    }
+    return str;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
