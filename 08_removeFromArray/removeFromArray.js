4df6d0ed9a18e491107ef8e0b9092dd7aba6be3c
David Blessing <8893118+macromore@users.noreply.github.com>
2025-10-25 22:13:16 -0400

    Complete ex08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ... item) {
+    for (let thing of item) {
+        for (let i = 0; i < arr.length; ) {
+            if (arr[i] === thing) {
+                arr.splice(i, 1);
+            } else {
+                i++;
+            };
+        };
+    };
+    return arr;
 };
 
 // Do not edit below this line
