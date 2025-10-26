86fb69394be039c739b575492ae3e89c5732cc04
David Blessing <8893118+macromore@users.noreply.github.com>
2025-10-25 22:24:29 -0400

    Complete ex09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        if (a > b) {
+            let c = a;
+            a = b;
+            b = c;
+        };
+        for (let i = a; i <=b; i++) sum += i;
+        return sum;
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
