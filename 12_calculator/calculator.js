4c1e5f7d527e90039de8fd3343fae0f3c1655965
David Blessing <8893118+macromore@users.noreply.github.com>
2026-02-26 12:53:30 -0500

    Start exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.sum()
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  return a*b
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 0) {
+    return 1
+  } else {
+    return n*factorial(n-1)
+  }
 };
 
 // Do not edit below this line
